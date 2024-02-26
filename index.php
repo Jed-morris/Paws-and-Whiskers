8c91972ca408a8846fc83c3f2f168280d0ca92b2
Jed-morris <alegre.jed@gmail.com>
2024-02-26 15:12:07 +0800

    modified:   index.php

--- index.php
+++ index.php
@@ -130,9 +130,21 @@
         </div>
     </div>
 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <button onclick="scrollToTop()" id="scrollToTopButton" title="Go to top">Up</button>
+
+    <script>
+        function openMenu(menuName) {
+            var i, cardContainer, navLinks;
+            cardContainer = document.getElementsByClassName("card-container");
+            for (i = 0; i < cardContainer.length; i++) {
+                cardContainer[i].style.display = "none";
+            }
+            document.getElementById(menuName).style.display = "grid";
+        }
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+    </script>
 
     <script>
 	(function(d, s, id) {
@@ -156,17 +168,17 @@
     </script>
 
     <script>
-        function openMenu(menuName) {
-            var i, cardContainer, navLinks;
-            cardContainer = document.getElementsByClassName("card-container");
-            for (i = 0; i < cardContainer.length; i++) {
-                cardContainer[i].style.display = "none";
-            }
-            document.getElementById(menuName).style.display = "grid";
-        }
-        // Get the element with id="defaultOpen" and click on it
-        document.getElementById("defaultOpen").click();
+        function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
     </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <script src="js/index.js"></script>
 
