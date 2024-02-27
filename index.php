4d2801d3e85f2da21e385864db640c80ca143a2a
JaRoChuaskii <148548328+JaRoChuaskii@users.noreply.github.com>
2024-02-27 19:40:21 +0800

    updating

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">    
     <link rel="stylesheet" href="styles.css">
-
 </head>
 <body class="bg-body-tertiary">
     <header class="h-auto">
