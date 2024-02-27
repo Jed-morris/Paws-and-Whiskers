ff1d66fb3077663873588512b254b31d6b3c1389
JaRoChuaskii <robertchua543@gmail.com>
2024-02-27 23:14:21 +0800

    renewing files

---
+++ db_conn.php
@@ -0,0 +1,20 @@
+<?php
+    class Database {
+        private $host = 'localhost';
+        private $username = 'root';
+        private $password = '';
+        private $database = 'mymp3_db';
+        private $connection;
+
+        public function __construct() {
+            $this->connection = new Mysqli($this->host, $this->username, $this->password, $this->database);
+            if ($this->connection->connect_error) {
+                die("Connection failed: " . $this->connection->connect_error);
+            }
+        }
+        
+        public function getConnection() {
+            return $this->connection;
+        }
+    }
+?>
